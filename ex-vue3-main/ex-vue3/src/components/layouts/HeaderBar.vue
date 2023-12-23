e7938ca86f6f6e213f8ca3fbe21acf8eece4c8ae
jmjmc <jimin.choi@cyberone.kr>
2023-12-23 23:13:49 +0900

    4
    4

--- ex-vue3-main/ex-vue3/src/components/layouts/HeaderBar.vue
+++ ex-vue3-main/ex-vue3/src/components/layouts/HeaderBar.vue
@@ -1,44 +1,45 @@
 <template>
   <div>
-    <header class="navbar navbar-expand-sm sticky-top bg-dark flex-md-nowrap p-0 shadow" data-bs-theme="dark">
-      <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white" href="#">Company name</a>
-      <ul class="navbar-nav flex-row d-md-none">
-        <li class="nav-item text-nowrap">
-          <button
-            class="nav-link px-3 text-white"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSearch"
-            aria-controls="navbarSearch"
-            aria-expanded="false"
-            aria-label="Toggle search"
-          >
-            <svg class="bi"><use xlink:href="#search" /></svg>
+    <header>
+      <nav class="navbar bg-dark navbar-expand-sm bg-body-tertiary border-bottom border-body fixed-top" data-bs-theme="dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Company name</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
           </button>
-        </li>
-        <li class="nav-item text-nowrap">
-          <button
-            class="nav-link px-3 text-white"
-            type="button"
-            data-bs-toggle="offcanvas"
-            data-bs-target="#sidebarMenu"
-            aria-controls="sidebarMenu"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <svg class="bi"><use xlink:href="#list" /></svg>
-          </button>
-        </li>
-      </ul>
-
-      <div id="navbarSearch" class="navbar-search w-100 collapse">
-        <input
-          class="form-control w-100 rounded-0 border-0"
-          type="text"
-          placeholder="Search"
-          aria-label="Search"
-        />
-      </div>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">회원관리</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">주문관리</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">배송관리</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">주문관리</a></li>
+                  <li><a class="dropdown-item" href="#">배송관리</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+              </li>
+            </ul>
+            <form class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
     </header>
     <main>
       <div class="container py-4">
@@ -48,6 +49,20 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.navbar {
+      width: 1700px;
+      margin: 0 auto;
+      height: 80px; /* 상단의 높이에 따라 조절하세요 */
+    }
+ /* 576px에서 991px 사이일 때의 스타일 */
+ @media (min-width: 576px) and (max-width: 991px) {
+    .navbar {
+      width: 80%; /* 또는 다른 원하는 값 */
+    }
+  }
+
+</style>
--- ex-vue3-main/ex-vue3/src/components/layouts/SideBar.vue
+++ ex-vue3-main/ex-vue3/src/components/layouts/SideBar.vue
@@ -1,124 +1,51 @@
 <template>
-  <div>
-    <div class="container-fluid">
-      <div class="row">
-        <div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
-          <div
-            class="offcanvas-md offcanvas-end bg-body-tertiary"
-            tabindex="-1"
-            id="sidebarMenu"
-            aria-labelledby="sidebarMenuLabel"
-          >
-            <div class="offcanvas-header">
-              <h5 class="offcanvas-title" id="sidebarMenuLabel">Company name</h5>
-              <button
-                type="button"
-                class="btn-close"
-                data-bs-dismiss="offcanvas"
-                data-bs-target="#sidebarMenu"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
-              <ul class="nav flex-column">
-                <li class="nav-item">
-                  <a
-                    class="nav-link d-flex align-items-center gap-2 active"
-                    aria-current="page"
-                    href="#"
-                  >
-                    <svg class="bi"><use xlink:href="#house-fill" /></svg>
-                    Dashboard
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link d-flex align-items-center gap-2" href="#">
-                    <svg class="bi"><use xlink:href="#file-earmark" /></svg>
-                    Orders
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link d-flex align-items-center gap-2" href="#">
-                    <svg class="bi"><use xlink:href="#cart" /></svg>
-                    Products
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link d-flex align-items-center gap-2" href="#">
-                    <svg class="bi"><use xlink:href="#people" /></svg>
-                    Customers
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link d-flex align-items-center gap-2" href="#">
-                    <svg class="bi"><use xlink:href="#graph-up" /></svg>
-                    Reports
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link d-flex align-items-center gap-2" href="#">
-                    <svg class="bi"><use xlink:href="#puzzle" /></svg>
-                    Integrations
-                  </a>
-                </li>
-              </ul>
-
-              <h6
-                class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-body-secondary text-uppercase"
+  <div class="container">
+    <div class="sidebar">
+      <div class="col-md-4 fixed-nav">
+          <ul class="nav flex-column">
+            <li class="nav-item md-1">
+              <a
+                class="nav-link active"
+                aria-current="page"
+                href="#"
               >
-                <span>Saved reports</span>
-                <a class="link-secondary" href="#" aria-label="Add a new report">
-                  <svg class="bi"><use xlink:href="#plus-circle" /></svg>
-                </a>
-              </h6>
-              <ul class="nav flex-column mb-auto">
-                <li class="nav-item">
-                  <a class="nav-link d-flex align-items-center gap-2" href="#">
-                    <svg class="bi"><use xlink:href="#file-earmark-text" /></svg>
-                    Current month
+                <svg class="bi" style="width: 1rem; height: 10rem;"><use xlink:href="#house-fill" /></svg>
+                회원관리
+              </a>
+            </li>
+            <li class="nav-item md-1">
+              <a class="nav-link" href="#">
+                <svg class="bi" style="width: 1rem; height: 5rem;"><use xlink:href="#file-earmark" /></svg>
+                주문관리
+              </a>
+            </li>
+            <li class="nav-item md-1">
+              <svg class="bi" style="width: 1rem; height: 5rem;"><use xlink:href="#cart" /></svg>
+              배송관리
+              <ul class="nav flex-column ml-3">
+                <li class="nav-item"> 
+                  <a class="nav-link" href="#">
+                  <svg class="bi" style="width: 1rem; height: 5rem;"><use xlink:href="#cart" /></svg>
+                    배송처리
                   </a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link d-flex align-items-center gap-2" href="#">
-                    <svg class="bi"><use xlink:href="#file-earmark-text" /></svg>
-                    Last quarter
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link d-flex align-items-center gap-2" href="#">
-                    <svg class="bi"><use xlink:href="#file-earmark-text" /></svg>
-                    Social engagement
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link d-flex align-items-center gap-2" href="#">
-                    <svg class="bi"><use xlink:href="#file-earmark-text" /></svg>
-                    Year-end sale
+                  <a class="nav-link" href="#">
+                  <svg class="bi" style="width: 1rem; height: 5rem;"><use xlink:href="#cart" /></svg>
+                  배송설정
                   </a>
                 </li>
               </ul>
-
-              <hr class="my-3" />
-
-              <ul class="nav flex-column mb-auto">
-                <li class="nav-item">
-                  <a class="nav-link d-flex align-items-center gap-2" href="#">
-                    <svg class="bi"><use xlink:href="#gear-wide-connected" /></svg>
-                    Settings
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link d-flex align-items-center gap-2" href="#">
-                    <svg class="bi"><use xlink:href="#door-closed" /></svg>
-                    Sign out
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
+            </li>
+            <li class="nav-item ">
+              <a class="nav-link" href="#">
+                <svg class="bi" style="width: 1rem; height: 10rem;"><use xlink:href="#people" /></svg>
+                클라임관리
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
-    </div>
     <main>
       <div class="container py-4">
         <RouterViwe></RouterViwe>
@@ -127,6 +54,20 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.fixed-nav {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 200px; /* 원하는 너비로 조절 */
+      height: 100vh; /* 전체 높이로 조절 */
+      background-color: #f8f9fa; /* 배경색 지정 */
+      padding-top: 20px; /* 상단 여백 조절 */
+    }
+  .bi{
+
+  }
+</style>
